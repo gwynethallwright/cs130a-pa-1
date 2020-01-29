92ec5c11313863b229d42dc6e9ee19cac7a03d18
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-01-29 14:13:30 -0800

    Polynomial addition working.

--- array.cpp
+++ array.cpp
@@ -3,6 +3,15 @@
 #include <string>
 using namespace std;
 
+int * addPoly(int coeffs[2][100]){
+  static int poly[100] = {};
+  for (int i = 0; i < 100; ++i){
+    poly[i] = coeffs[0][i] + coeffs[1][i];
+    cout << poly[i] << "\n";
+  }
+  return poly;
+}
+
 int * readPoly(string input){
   static int poly[100] = {};
   istringstream iss(input);
@@ -21,7 +30,9 @@ int main(int argc, char** argv){
   string current;
   int power;
   int poly[2][100] = {};
+  int * poly_result;
   int i = 0;
+  int add = 1;
   istringstream iss(argv[1]);
   do{
     iss >> current;
@@ -30,11 +41,13 @@ int main(int argc, char** argv){
     }
     else if (current == "*"){
       i = i+1;
+      add = 0;
     }
     else {
       iss >> power;
       poly[i][stoi(current)] = power;
     }
   } while (iss);
+  poly_result = addPoly(poly);
   return 0;
 }
